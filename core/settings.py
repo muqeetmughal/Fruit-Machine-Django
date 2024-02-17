46fe28e251c02a330dd03ea33322c052c308dff8
muqeetmughal <muqeetmughal786@gmail.com>
2024-02-17 20:01:01 +0500

    code updated

--- core/settings.py
+++ core/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
-        'USER': 'db',
-        'PASSWORD': 'fiuhdbtr98',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db',
+#         'USER': 'db',
+#         'PASSWORD': 'fiuhdbtr98',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 FIXTURE_DIRS = (
     os.path.join(BASE_DIR, 'fixtures/'),
 )
